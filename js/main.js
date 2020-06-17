c8172f542b6429c16da2c874f9bca349ddbd4ed8
Michal Wasilewski <majorstopczyk@pop-os.localdomain>
2020-06-17 11:22:18 +0100

    first init nothing important

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const divEl = document.createElement('div');
+divEl.setAttribute('id', 'slider');
+
+main.appendChild(divEl);
+
+const imgEl = document.createElement('img');
+imgEl.setAttribute('class', 'image');
+
